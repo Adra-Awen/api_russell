0163e3285f8cdf8aed9f3f3f3f644fa2aad6a639
adraawen <bauvais.n@protonmail.com>
2026-01-28 16:03:13 +0100

    add views for catway list + catway form

--- api/app.js
+++ api/app.js
@@ -25,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/catways', catwaysRouter);
+app.use(express.urlencoded({extended: false}));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
--- api/routes/index.js
+++ api/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const usersService = require('../services/users');
+const Catway = require('../models/catway');
 
 router.get('/', (req, res) => {
   res.render('index', {
@@ -11,5 +12,49 @@ router.get('/', (req, res) => {
 
 router.post('/login', usersService.login);
 router.get('/logout', usersService.logout);
+
+router.get('/catways-page', async (req, res) => {
+  try {
+    const catways = await Catway.find();
+
+    res.render('catways', {
+      catways
+    });
+  } catch (error) {
+    res.status(500).json('Erreur Serveur');
+  }
+});
+
+router.get('/catways-page/new',(req, res) => {
+  res.render('catway-form', {error: null});
+});
+
+router.post('/catways-page/new', async (req, res) => {
+  const { catwayNumber, catwayType, catwayState } = req.body;
+
+  try {
+    const existing = await Catway.findOne({catwayNumber});
+
+    if (existing) {
+      return res.status(400).render('catway-form', {
+        error: "Un catway avec ce numéro existe déjà."
+      });
+    }
+
+    const catway = new Catway({
+      catwayNumber,
+      catwayType,
+      catwayState
+    });
+
+    await catway.save();
+
+    return res.redirect('/catways-page');
+  } catch (error) {
+    return res.status(500).render('catway-form', {
+      error: 'Erreur serveur lors de la création.'
+    });
+  }
+});
 
 module.exports = router;
